6368cf8e9d39f5204672f24c471fad7e7c141ab6
basofiibnu <>
2022-03-17 21:14:43 +0700

    added loading scrollbar and complete property detail implementation

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Box, Flex, Spacer, Text, Avatar } from '@chakra-ui/react';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
@@ -28,6 +28,109 @@ const PropertyDetails = ({
   return (
     <Box maxWidth={'1000px'} margin="auto" p="4">
       {photos && <ImageScrollbar data={photos} />}
+      <Box width="full" p="6">
+        <Flex
+          paddingTop={2}
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+          <Box>
+            <Avatar size="sm" src={agency?.logo?.url} />
+          </Box>
+        </Flex>
+        <Flex
+          alignItems="center"
+          p="1"
+          justifyContent="space-between"
+          w="250px"
+          color="blue.400"
+        >
+          {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)}{' '}
+          sqft <BsGridFill />
+        </Flex>
+        <Box marginTop={'2'}>
+          <Text fontSize="lg" marginBottom={'2'} fontWeight="bold">
+            {title}
+          </Text>
+          <Text lineHeight={'2'} color="gray.600">
+            {description}
+          </Text>
+        </Box>
+        <Flex
+          flexWrap={'wrap'}
+          textTransform="uppercase"
+          justifyContent={'space-between'}
+        >
+          <Flex
+            justifyContent={'space-between'}
+            w="400px"
+            borderBottom={'1px'}
+            borderColor="gray.100"
+            p="3"
+          >
+            <Text>Type</Text>
+            <Text fontWeight={'bold'}>{type}</Text>
+          </Flex>
+          <Flex
+            justifyContent={'space-between'}
+            w="400px"
+            borderBottom={'1px'}
+            borderColor="gray.100"
+            p="3"
+          >
+            <Text>Purpose</Text>
+            <Text fontWeight={'bold'}>{purpose}</Text>
+          </Flex>
+          {furnishingStatus && (
+            <Flex
+              justifyContent={'space-between'}
+              w="400px"
+              borderBottom={'1px'}
+              borderColor="gray.100"
+              p="3"
+            >
+              <Text>Furnishing Status</Text>
+              <Text fontWeight={'bold'}>{furnishingStatus}</Text>
+            </Flex>
+          )}
+        </Flex>
+        <Box>
+          {amenities.length && (
+            <Fragment>
+              <Text fontSize={'2xl'} fontWeight="black" marginTop={5}>
+                Amenities
+              </Text>
+              <Flex flexWrap={'wrap'}>
+                {amenities.map((item) =>
+                  item.amenities.map((amenity) => (
+                    <Text
+                      key={amenity.text}
+                      fontWeight="bold"
+                      color="blue.400"
+                      fontSize={'l'}
+                      p="2"
+                      bg={'gray.200'}
+                      m="1"
+                      borderRadius={5}
+                    >
+                      {amenity.text}
+                    </Text>
+                  )),
+                )}
+              </Flex>
+            </Fragment>
+          )}
+        </Box>
+      </Box>
     </Box>
   );
 };
