8eb13abd251e25a38e3f2f14a5fc474575e401a1
basofiibnu <>
2022-03-17 18:11:49 +0700

    implement navbar and footer

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+const Footer = () => {
+  return (
+    <Box
+      textAlign="center"
+      p="5"
+      color="gray.600"
+      borderTop="1px"
+      borderColor="gray.300"
+    >
+      2022 Realtor, Inc
+    </Box>
+  );
+};
+
+export default Footer;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
 import { Fragment } from 'react/cjs/react.production.min';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
@@ -10,9 +12,13 @@ const Layout = ({ children }) => {
         <title>Real Estate Apps</title>
       </Head>
       <Box maxWidth="1280px" m="auto">
-        <header>Navbar</header>
+        <header>
+          <Navbar />
+        </header>
         <main>{children}</main>
-        <footer>Footer</footer>
+        <footer>
+          <Footer />
+        </footer>
       </Box>
     </Fragment>
   );
